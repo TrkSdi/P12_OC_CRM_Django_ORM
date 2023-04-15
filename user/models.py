8135dd43b43d3cdeba1e09d04396d35b281eb2bd
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-04-15 15:10:20 +0200

    create custom user model

--- user/models.py
+++ user/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class CustomUser(AbstractUser):
+    
+    ROLE = (
+        ('Gestion', 'Gestion'),
+        ('Vente', 'Vente'),
+        ('Support', 'Support'),
+    )
+    
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE, null=True, blank=True)
+    #username = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.username
