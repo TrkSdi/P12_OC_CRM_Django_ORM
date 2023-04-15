3427feaca7cfc1fed3c2d7abad4a51d9edae157f
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-04-15 15:09:16 +0200

    connect PostgreSQL DB

--- Epic_Events/settings.py
+++ Epic_Events/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'Epic_Events.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EpicEvents',
+        'USER': 'admin_db',
+        'PASSWORD': '123456',
+        
     }
 }
 
