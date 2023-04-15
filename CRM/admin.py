734450be788149b994b4c2b824dda23ec83a4915
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-04-15 16:23:36 +0200

    fieldsets admin site

--- CRM/admin.py
+++ CRM/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Prospect, Contract, Event
 # Register your models here.
 
-admin.site.register(Client)
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Informations client", {"fields": ["first_name", "last_name", "email", "phone", "mobile"]}),
+        ("Date", {"fields": ["date_created", "date_updated"]}),
+        ("Contact", {"fields": ["sales_contact"]}),
+    ]
+    
+@admin.register(Prospect)
+class ProspectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Informations client", {"fields": ["first_name", "last_name", "email", "phone", "mobile"]}),
+        ("Date", {"fields": ["date_created", "date_updated"]}),
+        ("Contact", {"fields": ["sales_contact"]}),
+    ]
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Contrat", {"fields": ["client", "date_created", "date_updated", "status"]}),
+        ("Facturation", {"fields": ["amount", "payement_due"]})
+    ]
+    
+@admin.register(Event)
+class Eventadmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Evenement", {"fields": ["client", "date_created", "date_updated", "attendees", "event_status"]}),
+        ("Note", {"fields": ["note"]}),
+        ("Contact", {"fields": ["support_contact"]}),
+        
+    ]
--- CRM/models.py
+++ CRM/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from Epic_Events.settings import AUTH_USER_MODEL
 
 # Create your models here.
 
@@ -10,5 +11,36 @@ class Client(models.Model):
     mobile = models.CharField(max_length=10)
     date_created = models.DateField()
     date_updated = models.DateField()
-    #sales_contact
+    sales_contact = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     
+
+class Prospect(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10)
+    date_created = models.DateField()
+    date_updated = models.DateField()
+    sales_contact = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Contract(models.Model):
+    sales_contact = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_created = models.DateField()
+    date_updated = models.DateField()
+    status = models.BooleanField(default=False)
+    amount = models.FloatField(max_length=4)
+    payement_due = models.DateField()
+    
+
+class Event(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_created = models.DateField()
+    date_updated = models.DateField()
+    support_contact = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    event_status = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    attendees = models.IntegerField()
+    event_date = models.DateTimeField()
+    note = models.TextField(max_length=500)
